963da146eeeab0df8b33c74b94e7870f375de740
Joannis Orlandos <joannis@orlandos.nl>
2018-02-02 16:24:25 +0100

    vapor 3 ping

--- Vapor3/Sources/Vapor3/main.swift
+++ Vapor3/Sources/Vapor3/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+import Vapor
+
+let app = try Application()
+
+let router = try app.make(Router.self)
+
+router.get("ping") { req in
+    return "pong"
+}
+    
+router.get("ping2") { req in
+    return "pong" as StaticString
+}
+
+try app.run()
